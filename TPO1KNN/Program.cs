1f3da295b3960d63d9a741bdd583c5f4e72ec31f
charles baudy <charlesbaudy@gmail>
2022-03-13 15:16:50 -0400

    vote

--- TPO1KNN/Program.cs
+++ TPO1KNN/Program.cs
@@ -13,14 +13,47 @@ namespace TPO1KNN
 
             KNN knn = new KNN();
 
+            List<float> distances = new List<float>();
+
+            float distance = 0.0F;
+
             List<Diagnostic> diagnostics =new List<Diagnostic>();
+
+            List<bool> labels = new List<bool>();
 
             diagnostics = knn.ImportSamples("samples.csv");
 
-            foreach(Diagnostic item in diagnostics)
+            int n = diagnostics.Count;
+            //Console.WriteLine(n);
+            int i = 0;
+
+            Diagnostic diagnostic = new Diagnostic();
+
+            diagnostic.Cp = 3.0F;
+            diagnostic.Ca = 2.0F;
+            diagnostic.Oldpeak = 0.8F;
+            diagnostic.Thal = 2.0F;
+
+            for (i = 0; i < n; i++)
             {
-                item.PrintInfo();
+                distance = knn.EuclideanDistance(diagnostic, diagnostics[i]);
+                distances.Add(distance);
+                labels.Add(diagnostics[i].Label);
+                //Console.WriteLine(distance+" -> "+ diagnostics[i].Label);
             }
+
+            //Console.WriteLine("-------------------------");
+
+            knn.ShellSort(distances, labels);
+
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine(distances[i] + " -> " + labels[i]);
+            }
+
+            bool verdict = knn.Vote(labels);
+
+            Console.WriteLine("verdict = "+verdict);
         }
     }
 }
--- TPO1KNN/model/KNN.cs
+++ TPO1KNN/model/KNN.cs
@@ -50,7 +50,12 @@ namespace TPO1KNN.model
 
         public float ManhattanDistance(Diagnostic first_sample, Diagnostic second_sample)
         {
-            throw new NotImplementedException();
+            float cp = Math.Abs(first_sample.Cp - second_sample.Cp);
+            float ca = Math.Abs(first_sample.Ca - second_sample.Ca);
+            float thal = Math.Abs(first_sample.Thal - second_sample.Thal);
+            float oldPeak = Math.Abs(first_sample.Oldpeak - second_sample.Oldpeak);
+
+            return cp + ca + thal + oldPeak;
         }
 
         public bool Predict(Diagnostic sample_to_predict)
@@ -60,17 +65,85 @@ namespace TPO1KNN.model
 
         public void ShellSort(List<float> distances, List<bool> labels)
         {
-            throw new NotImplementedException();
+            int n = distances.Count-1;
+            int h = 1;
+            int i = 0;
+            int j = 0;
+
+            while (h < n/3)
+            {
+                h = 3 * h + 1;
+            }
+
+            while (h >= 1)
+            {
+                for (i = h + 1; i <= n; i++)
+                {
+                    float tmp = distances[i];
+                    bool tmpLabel = labels[i];
+                    j = i;
+                    while(j-h>=1 && distances[j - h] > tmp)
+                    {
+                        labels[j] = labels[j - h];
+                        distances[j] = distances[j - h];
+                        j = j - h;
+                    }
+                    labels[j] = tmpLabel;
+                    distances[j] = tmp;
+                }
+                h = h / 3;
+            }
         }
 
         public void Train(string filename_train_samples_csv, int k = 1, int distance = 1)
         {
-            throw new NotImplementedException();
+            List<Diagnostic> diagnostics = this.ImportSamples(filename_train_samples_csv);
+
+            int n = diagnostics.Count;
+
+            Diagnostic diagnostic = new Diagnostic();
+
+            for(int x=0;x<n;x++)
+                for(int y=0;y<n;y++)
+                    for (int z = 0; z < n; z++)
+                        for (int t = 0; t < n; t++)
+                        {
+                            diagnostics[x].Cp = x*10;
+                            diagnostics[x].Ca = y*10;
+                            diagnostics[x].Oldpeak = z*10;
+                            diagnostics[x].Thal = t*10;
+                            EuclideanDistance(first_sample,);
+                        }
+
+
         }
 
         public bool Vote(List<bool> sorted_labels)
         {
-            throw new NotImplementedException();
+            int k = 1;
+            int cptPositive = 0;
+            int cptNegative = 0;
+
+            for (int i = 0; i <= k; i++)
+            {
+                if (sorted_labels[i] == true)
+                {
+                    cptPositive++;
+                } else
+                {
+                    cptNegative++;
+                }
+            }
+            if(cptNegative>cptPositive)
+            {
+                return false;
+            } else if (cptNegative < cptPositive)
+            {
+                return true;
+            } else
+            {
+                return true;
+            }
         }
     }
 }
